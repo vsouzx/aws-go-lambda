6ccf0ad94d70f2694636d67e2d25e0c876a82cad
vsouzx_ <vtsoliveira2001@gmail.com>
2025-06-14 02:04:16 -0300

    fix: ajustando gtw.tf

--- infra/gtw.tf
+++ infra/gtw.tf
@@ -7,7 +7,6 @@ resource "aws_api_gateway_rest_api" "hello_gw_api"{
     }
 }
 
-### REGISTER ENDPOINT ###
 resource "aws_api_gateway_resource" "hello_gw_api_resource" {
     parent_id       = aws_api_gateway_rest_api.hello_gw_api.root_resource_id
     path_part       = "hello"
@@ -24,16 +23,16 @@ resource "aws_api_gateway_method" "hello_gw_api_method_get" {
 resource "aws_api_gateway_integration" "hello_lambda_integration_get" {
     http_method = aws_api_gateway_method.hello_gw_api_method_get.http_method
     resource_id = aws_api_gateway_resource.hello_gw_api_resource.id
-    rest_api_id = aws_api_gateway_rest_api.facial_recognition_gw_api.id
+    rest_api_id = aws_api_gateway_rest_api.hello_gw_api.id
     type        = "AWS_PROXY"
 
     integration_http_method     = "POST" #para lambda_proxy, sempre deve ser POST
     uri = aws_lambda_function.lambda.invoke_arn
 }
 
-resource "aws_api_gateway_method_response" "register_response_200_post" {
-  rest_api_id = aws_api_gateway_rest_api.facial_recognition_gw_api.id
-  resource_id = aws_api_gateway_resource.register_gw_api_resource.id
+resource "aws_api_gateway_method_response" "hello_response_200_get" {
+  rest_api_id = aws_api_gateway_rest_api.hello_gw_api.id
+  resource_id = aws_api_gateway_resource.hello_gw_api_resource.id
   http_method = aws_api_gateway_method.hello_gw_api.http_method
   status_code = "200"
 }
