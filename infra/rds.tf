ed131325cb6ba9daee7cc86e5dacb4acba035cd8
vsouzx_ <vtsoliveira2001@gmail.com>
2025-06-15 18:14:24 -0300

    fix: ajustando engine_version ans instance_class

--- infra/rds.tf
+++ infra/rds.tf
@@ -2,8 +2,8 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 10
   db_name              = "mydb"
   engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t2.micro"
+  engine_version       = "8.0.41"
+  instance_class       = "db.t3.micro"
   username             = local.db_secret.db_username
   password             = local.db_secret.db_password
   parameter_group_name = "default.mysql8.0"
