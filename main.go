e3ff450cd29daf2b85716d87ffb8878fb73ed4f8
vsouzx_ <vtsoliveira2001@gmail.com>
2025-06-15 18:39:56 -0300

    recriando recursos

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -12,6 +14,14 @@ func main() {
 }
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	dbUser := os.Getenv("DB_USERNAME")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbuRL := os.Getenv("DB_URL")
+
+	fmt.Println("Usuário:", dbUser)
+	fmt.Println("Senha:", dbPass)
+	fmt.Println("URL do banco de dados:", dbuRL)
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "Hello, World!",
