53b4da1665c8b3d17c2aa2c6150f6f791b79eeaf
vsouzx_ <vtsoliveira2001@gmail.com>
2025-06-15 18:09:09 -0300

    fix: ajustando chave sm

--- infra/rds.tf
+++ infra/rds.tf
@@ -4,8 +4,8 @@ resource "aws_db_instance" "default" {
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = "db.t2.micro"
-  username             = local.db_secret.username
-  password             = local.db_secret.password
+  username             = local.db_secret.db_username
+  password             = local.db_secret.db_password
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
 }
