4eb0e7ef3dbff52905ee7dd865e7d9c2cabc3785
vsouzx_ <vtsoliveira2001@gmail.com>
2025-06-15 21:18:22 -0300

    ajustando makefile

--- app/setup-local.sh
+++ app/setup-local.sh
@@ -1,4 +1,4 @@
-export DB_USERNAME=admin
-export DB_PASSWORD=adminvs#
-export DB_URL=mydb-instance.cnhop5ivkd4i.us-east-1.rds.amazonaws.com:3306
+export DB_USERNAME=root
+export DB_PASSWORD=Vs368797@
+export DB_URL=localhost:3306
 export DB_NAME=mydb
