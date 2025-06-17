f7f140c4e95cc034d58c96e4d0fff4ffbeb2a381
vsouzx_ <vtsoliveira2001@gmail.com>
2025-06-16 23:55:08 -0300

    fix: fixing main.go

--- app/main.go
+++ app/main.go
@@ -2,37 +2,16 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"souzalambdago/config"
 	"souzalambdago/factory"
 	"souzalambdago/repository"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
-func main() {
-    mockRequest := events.APIGatewayProxyRequest{
-        QueryStringParameters: map[string]string{
-            "from_user_id": "5",
-        },
-        Headers: map[string]string{
-            "Authorization": "Bearer fake-token",
-        },
-        Path:       "/payment",
-        HTTPMethod: "GET",
-        Body:       `{
-            "from_user_id": 1,
-            "to_user_id": 2,
-            "amount": 100.50,
-            "type": "TRANSFER",
-            "description": "Payment for services"
-        }`,
-    }
 
-    response, err := handler(context.Background(), mockRequest)
-    if err != nil {
-        fmt.Println("Error:", err.Error())
-    }
-    fmt.Println("Response:", response)
+func main() {
+	lambda.Start(handler)
 }
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
