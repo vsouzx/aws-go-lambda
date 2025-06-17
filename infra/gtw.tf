cc534e49b184c8e46bb807f2944e71def984236e
vsouzx_ <vtsoliveira2001@gmail.com>
2025-06-17 00:11:03 -0300

    feat: subindo patch gtw

--- infra/gtw.tf
+++ infra/gtw.tf
@@ -63,6 +63,31 @@ resource "aws_api_gateway_method_response" "transaction_response_200_post" {
   status_code = "200"
 }
 
+//PATCH /transaction
+resource "aws_api_gateway_method" "transaction_gw_api_method_patch" {
+    authorization   = "NONE"
+    http_method     = "PATCH"
+    resource_id     = aws_api_gateway_resource.transaction_gw_api_resource.id
+    rest_api_id     = aws_api_gateway_rest_api.transaction_gw_api.id
+}
+
+resource "aws_api_gateway_integration" "transaction_lambda_integration_patch" {
+    http_method = aws_api_gateway_method.transaction_gw_api_method_patch.http_method
+    resource_id = aws_api_gateway_resource.transaction_gw_api_resource.id
+    rest_api_id = aws_api_gateway_rest_api.transaction_gw_api.id
+    type        = "AWS_PROXY"
+
+    integration_http_method     = "POST" #para lambda_proxy, sempre deve ser POST
+    uri = aws_lambda_function.lambda.invoke_arn
+}
+
+resource "aws_api_gateway_method_response" "transaction_response_200_patch" {
+  http_method = aws_api_gateway_method.transaction_gw_api_method_patch.http_method
+  resource_id = aws_api_gateway_resource.transaction_gw_api_resource.id
+  rest_api_id = aws_api_gateway_rest_api.transaction_gw_api.id
+  status_code = "200"
+}
+
 resource "aws_api_gateway_deployment" "api_deployment" {
     rest_api_id = aws_api_gateway_rest_api.transaction_gw_api.id
 
